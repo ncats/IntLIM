99757321c01694c769bfec1ea17966c2233bb96b
mapleknight <mapleknight@users.noreply.github.com>
2020-06-15 16:50:22 -0400

    Update install.R

--- install.R
+++ install.R
@@ -3,8 +3,3 @@ library(devtools)
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 BiocManager::install("MultiDataSet", update = TRUE)
-
-update.packages(ask = FALSE)
-
-install_github("ncats/IntLIM", force = TRUE)
-library(IntLIM)
