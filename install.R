d2a1f727bcd9254d27233a3b697c763067ff7718
mapleknight <mapleknight@users.noreply.github.com>
2020-06-15 14:47:30 -0400

    fix: updated installed packages in install.R

--- install.R
+++ install.R
@@ -4,5 +4,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 BiocManager::install("MultiDataSet", update = TRUE)
 
+update.packages(ask = FALSE)
+
 install_github("ncats/IntLIM", force = TRUE)
 library(IntLIM)
