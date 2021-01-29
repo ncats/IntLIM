43e4f6b72c423e55a7233c68ab2383573b7b23a2
Your Name <you@example.com>
2021-01-28 20:41:38 -0500

    Added global co-regulation capability

--- R/03_RunIntLim.R
+++ R/03_RunIntLim.R
@@ -88,3 +88,96 @@ RunIntLim <- function(inputData,stype=NULL,outcome="metabolite", covar=NULL,
     }
     return(myres)
 }
+
+#' Run linear models and retrieve relevant statistics for global co-regulation
+#'
+#' @include internalfunctions.R
+#'
+#' @param inputData MultiDataSet object (output of ReadData()) with gene expression,
+#' metabolite abundances, and associated meta-data
+#' @param stype column name that represents sample type (by default, it will be used
+#' in the interaction term). Only 2 categories are currently supported.
+#' @param outcome 'metabolite' or 'gene' must be set as outcome/independent variable
+#' (default is 'metabolite')
+#' @param covar Additional variables from the phenotypic data that be integrated into linear model
+#' @param class.covar Describing whether additional variables are 'numeric' or 'categorial'
+#' @param continuous boolean to indicate whether the data is continuous or discrete
+#' @param metabolite.pairs boolean to indicate whether to return metabolite-metabolite pairs (TRUE) or gene-metabolite pairs (FALSE)
+#' @param save.covar.pvals boolean to indicate whether or not to save the p-values of all covariates,
+#' which can be analyzed later but will also lengthen computation time. The default is FALSE.
+#' @param independent.var.type 'metabolite' or 'gene' must be set as independent variable
+#' (default is 'metabolite')
+#' 
+#' @return IntLimModel object with model results
+#'
+#' @examples
+#' dir <- system.file("extdata", package="IntLIM", mustWork=TRUE)
+#' csvfile <- file.path(dir, "NCItestinput.csv")
+#' mydata <- ReadData(csvfile,metabid='id',geneid='id')
+#' \dontrun{
+#' myres <- RunGlobalCoRegulation(mydata,stype="PBO_vs_Leukemia")
+#' }
+#' @export
+RunGlobalCoRegulation <- function(inputData,stype=NULL,outcome="metabolite", covar=NULL, 
+                      class.covar=NULL, continuous = FALSE, metabolite.pairs=FALSE,
+                      save.covar.pvals=FALSE, independent.var.type="gene"){
+    
+    
+    if (class(inputData) != "MultiDataSet") {
+        stop("input data is not a MultiDataSet class")
+    }
+    mytypes <- names(Biobase::assayData(inputData))
+    if(!any(mytypes=="expression") || !any(mytypes=="metabolite")) {
+        stop("input data must contain assayData of type 'metabolite' and 'expression.
+        Try reading in the data with the ReadData function")
+    }
+    if (is.null(stype)) {stop("Please set the variable type (e.g. sample group)")}
+    
+    incommon <- getCommon(inputData,stype,covar,class.covar=class.covar)
+    
+    if(!continuous & length(unique(stats::na.omit(incommon$p))) != 2) {
+        stop(paste("IntLim currently requires only two categories.  Make sure the 
+	               column",stype,"only has two unique values"))
+    }
+    
+    print("Running the analysis on")
+    print(table(incommon$p))
+    
+    ptm <- proc.time()
+    
+    myres <- NULL
+    if(independent.var.type == "gene" && outcome == "metabolite"){
+        myres <- RunLM(incommon,outcome=outcome,type=incommon$p,covar=covar, 
+                       continuous = continuous, save.covar.pvals=save.covar.pvals,
+                       global = TRUE)
+    }else if(independent.var.type == "metabolite" && outcome == "gene"){
+        myres <- RunLM(incommon,outcome=outcome,type=incommon$p,covar=covar, 
+                       continuous = continuous, save.covar.pvals=save.covar.pvals,
+                       global = TRUE)
+    }else if(independent.var.type == "metabolite" && outcome == "metabolite"){
+        myres <- RunLMMetabolitePairs(incommon,type=incommon$p,covar=covar, 
+                                      continuous = continuous, save.covar.pvals=
+                                          save.covar.pvals, global = TRUE)
+    }else if(independent.var.type == "gene" && outcome == "gene"){
+        myres <- RunLMGenePairs(incommon,type=incommon$p,covar=covar, 
+                                continuous = continuous, save.covar.pvals = 
+                                    save.covar.pvals, global = TRUE)
+    }else{
+        print(paste("Error! independent.var.type and outcome.type must both be either",
+                    "gene or metabolite in RunGlobalCoRegulation"))
+    }
+    print(proc.time() - ptm)
+    myres@stype=stype
+    myres@outcome=outcome
+    
+    if(!is.null(covar)){
+        covariate <- colnames(incommon$covar_matrix)
+        class.var <- c()
+        for(i in 1:length(covar)){
+            class.var[i] <- class(incommon$covar_matrix[,i])
+        }
+        
+        myres@covar <- data.frame(covariate,class.var)
+    }
+    return(myres)
+}
