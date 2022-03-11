4f148359c8470ebd79573881fe8d12fe4a7b44c9
Tara Eicher <taraeicher@yahoo.com>
2022-03-11 10:34:36 -0500

    Cosmetic updates to Shiny app

--- inst/shinyApp/server.R
+++ inst/shinyApp/server.R
@@ -258,41 +258,6 @@ shinyServer(function(input, output, session) {
   scatterrows<-eventReactive(input$run5,{
       input$table_rows_selected
   })
-    
-  # # Display scatterplot to 'scatterplot' widget (discrete).
-  # discreteScatter<-renderUI({
-  #   # Obtain pairs.
-  #   a<-as.matrix(scatterrows())
-  #   pair1<-as.matrix(myres2()[a[1,],])
-  #   independentAnalyteOfInterest1<-pair1[,"Analyte1"]
-  #   outcomeAnalyteOfInterest1<-pair1[,"Analyte2"]
-  #   
-  #   # Plot.
-  #   p<-IntLIM::PlotPair(FmultiData(),
-  #                       myres(),
-  #                       independentAnalyteOfInterest=independentAnalyteOfInterest1,
-  #                       outcomeAnalyteOfInterest=outcomeAnalyteOfInterest1,
-  #                       outcome = 1,
-  #                       independentVariable = 2)
-  #   p <-htmltools::browsable(highcharter::hw_grid(p, ncol = 1, rowheight = 550))
-  # })
-  # 
-  # ## Display scatterplot to 'scatterplot' widget (continuous).
-  # continuousScatter<-renderPlot({
-  #   # Obtain pairs.
-  #   a<-as.matrix(scatterrows())
-  #   pair1<-as.matrix(myres2()[a[1,],])
-  #   independentAnalyteOfInterest1<-pair1[,"Analyte1"]
-  #   outcomeAnalyteOfInterest1<-pair1[,"Analyte2"]
-  #   
-  #   # Plot.
-  #   IntLIM::PlotPair(FmultiData(),
-  #                       myres(),
-  #                       independentAnalyteOfInterest=independentAnalyteOfInterest1,
-  #                       outcomeAnalyteOfInterest=outcomeAnalyteOfInterest1,
-  #                       outcome = 1,
-  #                       independentVariable = 2)
-  # })
   
   output$scatterplot<-renderPlot({
     # Obtain pairs.
