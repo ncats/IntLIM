73c3474623adf6a00af1f47492ba7f28900abbec
Tara Eicher <taraeicher@yahoo.com>
2022-07-30 10:38:15 -0400

    Moved default write directory from home to tmp

--- R/writingfunctions.R
+++ R/writingfunctions.R
@@ -5,21 +5,39 @@
 #'
 #' @return the filename of the CSV file with results named with cohort
 #' @export
-OutputData <- function (inputData,filename="~/output.zip"){
+OutputData <- function (inputData,filename=""){
+  
+  # Set up temporary directory if filename is not specified.
+  if(filename == ""){
+    tmp<- tempdir()
+    if(!file.exists(tmp)){
+      dir.create(tmp)
+    }
+    filename<-paste0(tmp, "/output.zip")
+  }
+  
+  # Write data.
   type1 <- inputData@analyteType1
   type2 <- inputData@analyteType2
 	phenoData <- inputData@sampleMetaData
-	utils::write.csv(type1,path.expand("~/AnalyteType1Data.csv"),quote=FALSE)
-  utils::write.csv(type2,path.expand("~/AnalyteType2Data.csv"),quote=FALSE)
-  utils::write.csv(phenoData,path.expand("~/MetaSamples.csv"),quote=FALSE)
+	tmp2<- tempdir()
+	if(!file.exists(tmp2)){
+	  dir.create(tmp2)
+	}
+	utils::write.csv(type1,paste0(tmp2, "/AnalyteType1Data.csv"),quote=FALSE)
+  utils::write.csv(type2,paste0(tmp2, "/AnalyteType2Data.csv"),quote=FALSE)
+	utils::write.csv(phenoData,paste0(tmp2, "/MetaSamples.csv"),quote=FALSE)
+	
+	# Move data to zipped folder.
 	utils::zip(zipfile=path.expand(filename),
-	           flags="-b ~",
-	           files=c(path.expand("~/AnalyteType1Data.csv"),
-	                   path.expand("~/AnalyteType2Data.csv"),
-	                   path.expand("~/MetaSamples.csv")))
-	file.remove(path.expand("~/AnalyteType1Data.csv"))
-	file.remove(path.expand("~/AnalyteType2Data.csv"))
-	file.remove(path.expand("~/MetaSamples.csv"))
+	           flags=paste("-b", tmp2),
+	                 files=c(paste0(tmp2, "/AnalyteType1Data.csv"),
+	                   paste0(tmp2, "/AnalyteType2Data.csv"),
+	                         paste0(tmp2, "/MetaSamples.csv")))
+	file.remove(paste0(tmp2, "/AnalyteType1Data.csv"))
+	                 file.remove(paste0(tmp2, "/AnalyteType2Data.csv"))
+	file.remove(paste0(tmp2, "/MetaSamples.csv"))
+	message(paste("Wrote results to", filename))
 }
 
 #' Output results into a zipped CSV file.  Results include gene and metabolite pairs, along with model interaction p-values, and correlations in each group being evaluated.
@@ -29,8 +47,18 @@ OutputData <- function (inputData,filename="~/output.zip"){
 #'
 #' @return the filename of the CSV file with results named with cohort
 #' @export
-OutputResults <- function (inputResults,filename="results.csv"){
-	utils::write.csv(inputResults,filename,quote=TRUE,row.names=FALSE)
+OutputResults <- function (inputResults,filename=""){
+  
+  # Set up temporary directory if filename is not specified.
+  if(filename == ""){
+    tmp<- tempdir()
+    if(!file.exists(tmp)){
+      dir.create(tmp)
+    }
+    filename<-paste0(tmp, "/results.csv")
+  }
+  
+  # Write data.
+  utils::write.csv(inputResults,filename,quote=TRUE,row.names=FALSE)
+  message(paste("Wrote results to", filename))
 }
-
-
